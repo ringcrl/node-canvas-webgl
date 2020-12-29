4cd593940f3efb0431b5677fbec4b84d35bcf8ea
Chenng <ringcrl@foxmail.com>
2020-12-29 16:29:50 +0800

    chore: format

--- test/pixi2.js
+++ test/pixi2.js
@@ -16,53 +16,53 @@ async function main() {
     height: screenHeight,
   });
   document.body.appendChild(app.view);
-  
-  const bgBuffer = fs.readFileSync(path.resolve(__dirname, './assets/bg_depth_blur.jpg'))
-  const bgPixels = await Jimp.read(bgBuffer)
-  const bgTexture = PIXI.Texture.fromBuffer(bgPixels.bitmap.data, bgPixels.bitmap.width, bgPixels.bitmap.height)
+
+  const bgBuffer = fs.readFileSync(path.resolve(__dirname, './assets/bg_depth_blur.jpg'));
+  const bgPixels = await Jimp.read(bgBuffer);
+  const bgTexture = PIXI.Texture.fromBuffer(bgPixels.bitmap.data, bgPixels.bitmap.width, bgPixels.bitmap.height);
   const bg = PIXI.Sprite.from(bgTexture);
   bg.width = app.screen.width;
   bg.height = app.screen.height;
   app.stage.addChild(bg);
-  
-  const littleDudesBuffer = fs.readFileSync(path.resolve(__dirname, './assets/depth_blur_dudes.jpg'))
-  const littleDudesPixels = await Jimp.read(littleDudesBuffer)
-  const littleDudesTexture = PIXI.Texture.fromBuffer(littleDudesPixels.bitmap.data, littleDudesPixels.bitmap.width, littleDudesPixels.bitmap.height)
+
+  const littleDudesBuffer = fs.readFileSync(path.resolve(__dirname, './assets/depth_blur_dudes.jpg'));
+  const littleDudesPixels = await Jimp.read(littleDudesBuffer);
+  const littleDudesTexture = PIXI.Texture.fromBuffer(littleDudesPixels.bitmap.data, littleDudesPixels.bitmap.width, littleDudesPixels.bitmap.height);
   const littleDudes = PIXI.Sprite.from(littleDudesTexture);
   littleDudes.x = (app.screen.width / 2) - 315;
   littleDudes.y = 200;
   app.stage.addChild(littleDudes);
-  
-  const littleRobotBuffer = fs.readFileSync(path.resolve(__dirname, './assets/depth_blur_moby.jpg'))
-  const littleRobotPixels = await Jimp.read(littleRobotBuffer)
-  const littleRobotTexture = PIXI.Texture.fromBuffer(littleRobotPixels.bitmap.data, littleRobotPixels.bitmap.width, littleRobotPixels.bitmap.height)
+
+  const littleRobotBuffer = fs.readFileSync(path.resolve(__dirname, './assets/depth_blur_moby.jpg'));
+  const littleRobotPixels = await Jimp.read(littleRobotBuffer);
+  const littleRobotTexture = PIXI.Texture.fromBuffer(littleRobotPixels.bitmap.data, littleRobotPixels.bitmap.width, littleRobotPixels.bitmap.height);
   const littleRobot = PIXI.Sprite.from(littleRobotTexture);
   littleRobot.x = (app.screen.width / 2) - 200;
   littleRobot.y = 100;
   app.stage.addChild(littleRobot);
-  
+
   const blurFilter1 = new PIXI.filters.BlurFilter();
   const blurFilter2 = new PIXI.filters.BlurFilter();
-  
+
   littleDudes.filters = [blurFilter1];
   littleRobot.filters = [blurFilter2];
-  
-  let count = 0;
-  
-  app.ticker.add(() => {
-      count += 0.005;
-  
-      const blurAmount = Math.cos(count);
-      const blurAmount2 = Math.sin(count);
-  
-      blurFilter1.blur = 20 * (blurAmount);
-      blurFilter2.blur = 20 * (blurAmount2);
 
-      console.time('toBuffer')
-      const canvasBuffer = app.view.toBuffer();
-      console.timeEnd('toBuffer')
-      fs.writeFileSync('./snapshot/pixi2.png', canvasBuffer);
-  });  
+  let count = 0;
+
+  app.ticker.add(() => {
+    count += 0.005;
+
+    const blurAmount = Math.cos(count);
+    const blurAmount2 = Math.sin(count);
+
+    blurFilter1.blur = 20 * (blurAmount);
+    blurFilter2.blur = 20 * (blurAmount2);
+
+    console.time('toBuffer');
+    const canvasBuffer = app.view.toBuffer();
+    console.timeEnd('toBuffer');
+    fs.writeFileSync('./snapshot/pixi2.png', canvasBuffer);
+  });
 }
 
 main();
--- test/pixi3.js
+++ test/pixi3.js
@@ -11,24 +11,27 @@ const screenWidth = 800;
 const screenHeight = 600;
 
 async function main() {
-  const app = new PIXI.Application();
+  const app = new PIXI.Application({
+    width: screenWidth,
+    height: screenHeight,
+  });
   document.body.appendChild(app.view);
-  
+
   const geometry = new PIXI.Geometry()
-      .addAttribute('aVertexPosition', // the attribute name
-          [-100, -100, // x, y
-              100, -100, // x, y
-              100, 100,
-              -100, 100], // x, y
-          2) // the size of the attribute
-      .addAttribute('aUvs', // the attribute name
-          [0, 0, // u, v
-              1, 0, // u, v
-              1, 1,
-              0, 1], // u, v
-          2) // the size of the attribute
-      .addIndex([0, 1, 2, 0, 2, 3]);
-  
+    .addAttribute('aVertexPosition', // the attribute name
+      [-100, -100, // x, y
+        100, -100, // x, y
+        100, 100,
+        -100, 100], // x, y
+      2) // the size of the attribute
+    .addAttribute('aUvs', // the attribute name
+      [0, 0, // u, v
+        1, 0, // u, v
+        1, 1,
+        0, 1], // u, v
+      2) // the size of the attribute
+    .addIndex([0, 1, 2, 0, 2, 3]);
+
   const vertexSrc = `
   
       precision mediump float;
@@ -47,7 +50,7 @@ async function main() {
           gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
   
       }`;
-  
+
   const fragmentSrc = `
   
       precision mediump float;
@@ -61,37 +64,37 @@ async function main() {
   
           gl_FragColor = texture2D(uSampler2, vUvs + sin( (time + (vUvs.x) * 14.) ) * 0.1 );
       }`;
-  
+
   const textureBuffer = fs.readFileSync(path.resolve(__dirname, './assets/bg_scene_rotate.jpg'));
   const jimp = await Jimp.read(textureBuffer);
   const texture = PIXI.Texture.fromBuffer(jimp.bitmap.data, jimp.bitmap.width, jimp.bitmap.height);
 
   const uniforms = {
-      uSampler2: texture,
-      time: 0,
+    uSampler2: texture,
+    time: 0,
   };
-  
+
   const shader = PIXI.Shader.from(vertexSrc, fragmentSrc, uniforms);
-  
+
   const quad = new PIXI.Mesh(geometry, shader);
-  
+
   quad.position.set(400, 300);
   quad.scale.set(2);
-  
+
   app.stage.addChild(quad);
-  
+
   // start the animation..
   // requestAnimationFrame(animate);
-  
-  app.ticker.add((delta) => {
-      quad.rotation += 0.01;
-      quad.shader.uniforms.time += 0.1;
 
-      console.time('toBuffer')
-      const canvasBuffer = app.view.toBuffer();
-      console.timeEnd('toBuffer')
-      fs.writeFileSync('./snapshot/pix3.png', canvasBuffer);
-  });  
+  app.ticker.add((delta) => {
+    quad.rotation += 0.01;
+    quad.shader.uniforms.time += 0.1;
+
+    console.time('toBuffer');
+    const canvasBuffer = app.view.toBuffer();
+    console.timeEnd('toBuffer');
+    fs.writeFileSync('./snapshot/pix3.png', canvasBuffer);
+  });
 }
 
 main();
